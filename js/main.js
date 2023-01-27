7f0ea3dab9517b35d1aa54ed87403820fd257a82
Toxa <suppertoha@yandex.ru>
2023-01-27 17:20:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  // ! Switch-el
+  $('.switch-el').on('click', function () {
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+
+  //! Burger
+
+  $('.top-header__burger').on('click', function () {
+    $(this).toggleClass('active');
+    $('.menu').toggleClass('active');
+  });
+
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.top-header__burger, .menu').length) {
+      $('.menu').removeClass('active');
+      $('.top-header__burger').removeClass('active');
+    }
+    e.stopPropagation();
+  });
+
+  //! ButtonDelivery
+  function buttonDelivery() {
+    $('.dropdown-main__button').on('click', function (e) {
+      e.preventDefault();
+      $('.dropdown-main__body').removeClass('active');
+      $(this).toggleClass('active');
+      $(this).next().toggleClass('active');
+    });
+
+    $('.dropdown-main__close').on('click', function () {
+      $('.dropdown-main__body').removeClass('active');
+    });
+
+    $(document).on('click', function (e) {
+      if (!$(e.target).closest('.dropdown-main__button, .dropdown-main__body').length) {
+        $('.dropdown-main__body').removeClass('active');
+        $('.dropdown-main__button').removeClass('active');
+      }
+      e.stopPropagation();
+    });
+  }
+  buttonDelivery();
+
+  //! Swiper
+  const swiperRegions = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
